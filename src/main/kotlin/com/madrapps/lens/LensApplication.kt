81a277ae20b9a545c55f83aeb58410650f2437a8
Henry <th.saravana@gmail.com>
2018-08-19 10:17:16 +0530

    Generated by start.spring.io

---
+++ src/main/kotlin/com/madrapps/lens/LensApplication.kt
@@ -0,0 +1,11 @@
+package com.madrapps.lens
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LensApplication
+
+fun main(args: Array<String>) {
+    runApplication<LensApplication>(*args)
+}
